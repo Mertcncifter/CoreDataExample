e26b7b6b5abd9823053ee40aee9bd6596ab7327e
mertcancifter <mert_1998_03@hotmail.com.tr>
2022-11-07 20:25:54 +0300

    Initial Commit

---
+++ HelloCoreDate/HelloCoreDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDateApp.swift
+//  HelloCoreDate
+//
+//  Created by mert can çifter on 7.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
